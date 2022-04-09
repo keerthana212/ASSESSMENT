58a8c28475e46d91e9081bb760ece036fff7a3a2
keerthana212 <102730993+keerthana212@users.noreply.github.com>
2022-04-09 09:44:51 +0530

    Add files via upload

---
+++ SI.java
@@ -0,0 +1,18 @@
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float p=13000.0f;
+		float t=2.0f;
+		float r=9.0f;
+		float sI;
+		sI=(p*r*t)/100;
+		System.out.println("SI:"+sI);
+		float totalAmount;
+		totalAmount=p*(1+r*t);
+		System.out.println("total amount:"+totalAmount);
+		
+
+	}
+
+}
